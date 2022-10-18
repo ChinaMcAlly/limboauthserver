0a995a217f9bbebdf1993e0a9360b3a627b7e31d
jja8 <2801819967@qq.com>
2022-10-18 09:03:37 +0800

    完成查询修改更新

--- src/main/java/cn/moonmc/ability/login/data/UserManager.java
+++ src/main/java/cn/moonmc/ability/login/data/UserManager.java
@@ -3,6 +3,7 @@ package cn.moonmc.ability.login.data;
 import cn.moonmc.ability.login.Login;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
@@ -43,8 +44,49 @@ public class UserManager {
     }
     /**
      * 更新用户数据到数据库
+     * @return 更新成功返回user 失败返回null
      * */
     public User update(User user){
+        try (
+                PreparedStatement p = login.getDataSource().getConnection().prepareStatement("update user set name=?,password=?,phone=?,ip=? where uuid=?");
+                ){
+            p.setString(1,user.name);
+            p.setString(2,user.password.ciphertext);
+            p.setLong(3,user.phone);
+            p.setString(4,user.ip);
+            p.setString(5,user.uuid.toString());
+            if (p.executeUpdate()>0) {
+                return user;
+            }else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    /**
+     * 通过uuid查询用户
+     * */
+    public User selectOfUUID(UUID uuid){
+        try (
+                PreparedStatement p = login.getDataSource().getConnection().prepareStatement("select uuid,name,password,phone,ip from user where uuid=?");
+                ){
+            p.setString(1,uuid.toString());
+            ResultSet r = p.executeQuery();
+            if (r.next()){
+                return new User(
+                        UUID.fromString(r.getString(1)),
+                        r.getString(2),
+                        r.getString(3),
+                        r.getLong(4),
+                        r.getString(5)
+                );
+            }else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
